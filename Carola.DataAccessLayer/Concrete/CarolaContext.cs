93ee8d295a81c7d2d4ece3f0bd2cc395c9dca41f
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-03-10 22:03:14 +0300

    ...

---
+++ Carola.DataAccessLayer/Concrete/CarolaContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carola.DataAccessLayer.Concrete
+{
+    internal class CarolaContext
+    {
+    }
+}
