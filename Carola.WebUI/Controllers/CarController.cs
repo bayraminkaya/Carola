4e55025cf8f3cc4e4d499df37f5a6dba21276957
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-03-12 22:06:20 +0300

    ...

---
+++ Carola.WebUI/Controllers/CarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carola.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult CarList()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCar()
+        {
+            return View();
+        }
+    }
+}
